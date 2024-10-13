831aaf3a82c21bcdad97d13fd6888b2143114d47
Amelia Alfonso <alfonsoa@bu.edu>
2024-10-13 18:01:32 -0400

    Added LSA search

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def search():
     return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices})
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(host='0.0.0.0', port=3000)
